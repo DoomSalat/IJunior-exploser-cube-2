649204703902ba053f861f167c8b7a422b737386
devSalat <kd.zaytcev@gmail.com>
2025-03-08 11:27:06 +0300

    Свойство Chance в Cube упрощено

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -8,23 +8,13 @@ public class Cube : MonoBehaviour
 
 	[SerializeField][Range(0, 100)] private float _chance;
 
-	public float Chance
-	{
-		get
-		{
-			return _chance;
-		}
-		private set
-		{
-			_chance = Mathf.Max(0, value);
-		}
-	}
+	public float Chance => _chance;
 
 	public Rigidbody SelfRigidbody => _selfRigidbody;
 
 	public void Construct(float chance, float scale)
 	{
-		Chance = chance;
+		_chance = Mathf.Max(0, chance);
 		transform.localScale = new Vector3(scale, scale, scale);
 
 		_selfRenderer.material = _randomMaterial.GetRandom();
